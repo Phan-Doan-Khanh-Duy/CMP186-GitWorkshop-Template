33f8010a0b43fcefb0d0295a16d2fb97e5aa58d2
User Name <phankhanhduy050600hiie@gmail.com>
2022-03-03 11:07:02 +0700

    Changle Salary

--- GitWorkshopProject/SalaryCalculator/EmployeeService.cs
+++ GitWorkshopProject/SalaryCalculator/EmployeeService.cs
@@ -21,13 +21,15 @@ namespace SalaryCalculator
             }
             else if (employeeType == EmployeeType.SeniorEngineer || employeeType == EmployeeType.Manager)
             {
-                factor = 1.5m;
+                factor = 2m;
             }
             else if (employeeType == EmployeeType.SeniorManager)
             {
                 factor = 3;
             }
             // TODO: if employee is Expert, set factor = 3
+
+            
 
             var salary = BASIC_SALARY * years * factor;
 
